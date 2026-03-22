571a892f712c37f54c980ae048041be8f9a83841
NovusBoard Dev <dev@novusboard.com>
2026-03-22 11:08:18 +0530

    Configure stable Android signature keys for Firebase Auth

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,19 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("debug") {
+            storeFile = file("debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
+
     buildTypes {
+        getByName("debug") {
+            signingConfig = signingConfigs.getByName("debug")
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
