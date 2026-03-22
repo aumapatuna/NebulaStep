ebcd6dec7b8d2a8016dba7937eb58a3c5a374aaf
NovusBoard Dev <dev@novusboard.com>
2026-03-22 11:37:03 +0530

    Fix duplicate Android debug signingConfig conflict

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
     }
 
     signingConfigs {
-        create("debug") {
+        getByName("debug") {
             storeFile = file("debug.keystore")
             storePassword = "android"
             keyAlias = "androiddebugkey"
